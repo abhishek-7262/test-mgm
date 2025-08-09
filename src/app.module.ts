035f5f1f0e9b9150b9d66d96c047e01db305ffdf
abhishek-7262 <abhishek.7262020269@gmail.com>
2025-08-09 19:38:12 +0530

    structure setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { QuestionsModule } from './questions/questions.module';
+import { TestsModule } from './tests/tests.module';
+import { ResultsModule } from './results/results.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, QuestionsModule, TestsModule, ResultsModule],
   controllers: [AppController],
   providers: [AppService],
 })
